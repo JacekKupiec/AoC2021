ecf5765c8c576a82f109bfa3088b158e0edefe49
Jacek Kupiec <jacek.kupiec@o2.pl>
2022-05-02 11:33:58 +0200

    task 10 part 2

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,41 +1,71 @@
 use std::collections::HashMap;
 use std::io;
 
+fn is_complementary_pair(left : char, right: char) -> bool {
+    return (left == '(' && right == ')')
+        || (left == '[' && right == ']')
+        || (left == '{' && right == '}')
+        || (left == '<' && right == '>');
+}
+
+fn get_complementary_character(c : char) -> char {
+    match c {
+        '(' =>  ')',
+        '{' => '}',
+        '[' => ']',
+        '<' => '>',
+        _ => panic!("Unexpected symbol")
+    }
+}
+
+fn is_valid_string(line: &str) -> Result<Vec<char>, String> {
+    let mut stack : Vec<char> = Vec::new();
+
+    for c in line.trim_end().chars() {
+        match c {
+            '(' | '[' | '{' | '<' => stack.push(c),
+            ')' | ']' | '}' | '>' => {
+                if let Some(ch) = stack.last() {
+                    if is_complementary_pair(*ch, c) {
+                        stack.pop();
+                    } else {
+                        return Err(String::from("Invalid string")); // Find the first occurrence of incorrect closing character
+                    }
+                }
+                else { // When there are more closing characters than opening ones
+                    return Err(String::from("Invalid string"));// Find the first occurrence of incorrect closing character
+                }
+            },
+            _ => panic!("Invalid character!")
+        }
+    }
+
+    return Ok(stack);
+}
+
 fn main() {
     let mut line = String::new();
-    let mut stack : Vec<char> = Vec::new();
-    let mut points_counter = 0;
-    let points_for_character = HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137)]);
+    let mut all_results = Vec::new();
 
     while let Ok(bytes_read) = io::stdin().read_line(&mut line) {
         if bytes_read == 0 {
             break;
         }
 
-        for c in line.trim_end().chars() {
-            match c {
-                '(' | '[' | '{' | '<' => stack.push(c),
-                ')' | ']' | '}' | '>' => {
-                    if let Some(ch) = stack.last() {
-                        if *ch == c {
-                            stack.pop();
-                        } else {
-                            points_counter += points_for_character[&c];
-                            break; // Find the first occurrence of incorrect closing character
-                        }
-                    }
-                    else { // When there are more closing characters than opening ones
-                        points_counter += points_for_character[&c];
-                        break;// Find the first occurrence of incorrect closing character
-                    }
-                },
-                _ => panic!("Niewłaściwy znak")
+        if let Ok(mut stack) = is_valid_string(&line) {
+            let chars_points = HashMap::from([('(', 1), ('[', 2), ('{', 3), ('<', 4)]);
+            let mut points_counter: u64 = 0;
+
+            while let Some(c) = stack.pop() {
+                points_counter = points_counter*5 + chars_points[&c];
             }
+
+            all_results.push(points_counter);
         }
 
         line.clear();
-        stack.clear();
     }
 
-    println!("{}", points_counter);
+    all_results.sort();
+    println!("{}", all_results[all_results.len() / 2]);
 }
