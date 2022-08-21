8867f95fc3bd4c08174fadcbf9e1bd3020870733
Jacek Kupiec <jacek.kupiec@o2.pl>
2022-08-21 12:03:35 +0200

    it works

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -4,27 +4,24 @@ use std::io;
 fn main() {
     let mut buffer = String::new();
     let input = io::stdin();
-    let mut graph: HashMap<&str, Vec<String>> = HashMap::new();
+    let mut graph: HashMap<String, Vec<String>> = HashMap::new();
 
     while let Ok(bytes_read) = input.read_line(&mut buffer) {
         if bytes_read == 0 {
             break;
         }
 
-        if let Some((verticle_name1, verticle_name2)) = buffer.split_once('-') {
-            let verticle_name_new1 = String::from(verticle_name1);
-            let verticle_name_new2 = String::from(verticle_name2);
-
-            if let Some(neighbour_list) = graph.get_mut(verticle_name_new1.as_str()) {
-                neighbour_list.push(verticle_name_new2);
+        if let Some((source_verticle_name, target_verticle_name)) = buffer.split_once('-') {
+            if let Some(neighbour_list) = graph.get_mut(source_verticle_name) {
+                neighbour_list.push(target_verticle_name.to_string());
             } else {
-                graph.insert(verticle_name1, vec![verticle_name_new2]);
+                graph.insert(source_verticle_name.to_string(), vec![target_verticle_name.to_string()]);
             }
 
-            if let Some(neighbour_list) = graph.get_mut(&verticle_name_new2.as_str()) {
-                neighbour_list.push(verticle_name_new1);
+            if let Some(neighbour_list) = graph.get_mut(target_verticle_name) {
+                neighbour_list.push(source_verticle_name.to_string());
             } else {
-                graph.insert(verticle_name2, vec![verticle_name_new1]);
+                graph.insert(target_verticle_name.to_string(), vec![source_verticle_name.to_string()]);
             }
         }
 
