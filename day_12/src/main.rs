b8ec4735bf0f3a8b58bd943dff5e351dd002b302
Jacek Kupiec <jacek.kupiec@o2.pl>
2022-08-24 22:42:28 +0200

    task 12 works

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,30 +1,50 @@
 use std::collections::{HashMap, HashSet};
 use std::io;
 
-fn dfs(graph: &HashMap<String, Vec<String>>, label: &str, visited: &mut HashSet<String>, counter: &mut i32, key: &str) {
+/*Zamiast kombinować z liczeniem odwiedziny po prostu dodaj jeszcze jeden wierzchołek*/
+
+fn is_node_auxiliary(node_name: &str) -> bool {
+    return node_name.ends_with("0");
+}
+
+fn dfs(graph: &HashMap<String, Vec<String>>, label: &str, visited: &mut HashSet<String>, is_auxiliary_node_used: bool) -> i32 {
     if label == "end" {
-        *counter = *counter + 1;
+        return 1;
     }
     else {
-        if label.chars().any(|c| c.is_lowercase()) {
+        let is_lowercase_node = label.chars().any(|c| c.is_lowercase());
+
+        if is_lowercase_node {
             visited.insert(label.to_string());
         }
 
+        let mut counter = 0;
+
         if let Some(neighbours) = graph.get(label) {
             for neighbour in neighbours {
-                if !visited.contains(neighbour) {
-                    dfs(graph, neighbour, visited, counter, key);
+                let is_neighbour_auxiliary_node = is_node_auxiliary(neighbour);
+                let can_visit_normal_node = !visited.contains(neighbour) && !is_neighbour_auxiliary_node;
+                let can_visit_auxiliary_node = is_neighbour_auxiliary_node
+                    && !is_auxiliary_node_used
+                    && visited.contains(&neighbour[0..(neighbour.len() - 1)]);
+
+                if can_visit_normal_node || can_visit_auxiliary_node {
+                    counter += dfs(graph, neighbour, visited, is_neighbour_auxiliary_node || is_auxiliary_node_used);
                 }
             }
         }
 
-        visited.remove(label);
+        if is_lowercase_node {
+            visited.remove(label);
+        }
+
+        return counter;
     }
 }
 
 fn main() {
-    let mut buffer = String::new();
     let input = io::stdin();
+    let mut buffer = String::new();
     let mut graph: HashMap<String, Vec<String>> = HashMap::new();
 
     while let Ok(bytes_read) = input.read_line(&mut buffer) {
@@ -32,39 +52,46 @@ fn main() {
             break;
         }
 
-        if let Some((source_verticle_name, target_verticle_name)) = buffer.trim_end().split_once('-') {
-            if let Some(neighbour_list) = graph.get_mut(source_verticle_name) {
-                neighbour_list.push(target_verticle_name.to_string());
+        if let Some((source_vertex_name, target_vertex_name)) = buffer.trim_end().split_once('-') {
+            if let Some(neighbour_list) = graph.get_mut(source_vertex_name) {
+                neighbour_list.push(target_vertex_name.to_string());
             } else {
-                graph.insert(source_verticle_name.to_string(), vec![target_verticle_name.to_string()]);
+                graph.insert(source_vertex_name.to_string(), vec![target_vertex_name.to_string()]);
             }
 
-            if let Some(neighbour_list) = graph.get_mut(target_verticle_name) {
-                neighbour_list.push(source_verticle_name.to_string());
+            if let Some(neighbour_list) = graph.get_mut(target_vertex_name) {
+                neighbour_list.push(source_vertex_name.to_string());
             } else {
-                graph.insert(target_verticle_name.to_string(), vec![source_verticle_name.to_string()]);
+                graph.insert(target_vertex_name.to_string(), vec![source_vertex_name.to_string()]);
             }
         }
 
         buffer.clear();
     }
 
-    println!("Wczytany graf:\n{:?}", graph);
+    let lowercase_nodes : Vec<_> = graph.keys()
+        .filter(|label| label.chars().any(|c| c.is_lowercase()) && *label != "start" && *label != "end")
+        .map(|label| label.to_string())
+        .collect();
 
-    let mut visited : HashSet<String> = HashSet::new();
-    let mut counter_all_cases = 0;
-    let can_be_visited_twice = graph.keys().filter(|key|
-            key.chars().any(|c|
-                    c.is_lowercase())
-                    && *key != "end"
-                    && *key != "start");
+    for label in lowercase_nodes {
+        let neighbours = graph[&label].clone();
 
-    for key in can_be_visited_twice {
-        let mut counter = 0;
-        dfs(&graph, "start", &mut visited, &mut counter, key);
+        for neighbour in neighbours.iter() {
+            let new_label = format!("{}{}", label, 0);
+            let neighbours_of_neighbour = graph.get_mut(neighbour).unwrap();
 
-        counter_all_cases += counter;
+            neighbours_of_neighbour.push(new_label);
+        }
+
+        let new_label = format!("{}{}", label, 0);
+        graph.insert(new_label, neighbours.clone());
     }
 
-    println!("Różne ścieżki: {}", counter_all_cases);
+    println!("Wczytany graf z modyfikacjami:\n{:?}", graph);
+
+    let mut visited : HashSet<String> = HashSet::new();
+    let counter = dfs(&graph, "start", &mut visited, false);
+
+    println!("Różne ścieżki: {}", counter);
 }
