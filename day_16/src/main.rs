acabe4ce6d48a6f4d25647478392ed789ab37586
Jacek Kupiec <jacek.kupiec@o2.pl>
2022-09-19 16:01:31 +0200

    read input

--- day_16/src/main.rs
+++ day_16/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+
 fn main() {
-    let mut buffer = String::new();
+    let input = File::open("D:\\source\\Rust\\AoC 2021\\day_16\\src\\big_input.txt").unwrap();
+    let mut reader = BufReader::new(input);
+    let mut buffer = [0; 400];
+
+    reader.read(&mut buffer).expect("Podany plik z danymi wejściowymi nie istnieje");
+
+    for element in &mut buffer {
+        match *element {
+            b'0'..=b'9' => *element -= b'0',
+            b'A'..=b'F' => *element -= b'A' - 10,
+            _ => panic!("Unexpected character found: {}", *element)
+        }
+    }
+
+    let mut bit_position = 0;
+
+    loop {
+        
+    }
 }
