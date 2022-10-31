01476be46fb79cc654b651871d41c37dcd62fe9f
Jacek Kupiec <jacek.kupiec@o2.pl>
2022-10-31 16:48:43 +0100

    start day 17

---
+++ day_17/src/main.rs
@@ -0,0 +1,10 @@
+/* target area: x=185..221, y=-122..-74
+    The probe's x position increases by its x velocity.
+    The probe's y position increases by its y velocity.
+    Due to drag, the probe's x velocity changes by 1 toward the value 0; that is, it decreases by 1 if it is greater than 0, increases by 1 if it is less than 0, or does not change if it is already 0.
+    Due to gravity, the probe's y velocity decreases by 1.
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
